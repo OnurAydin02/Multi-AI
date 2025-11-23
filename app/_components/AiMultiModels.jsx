80a41cc1e2cc47998ad5d1feb141eadae9f9cda5
OnurAydin02 <07onuraydin@gmail.com>
2025-11-23 19:28:06 +0300

    Saved User Pref To Firebase for AI Models

--- app/_components/AiMultiModels.jsx
+++ app/_components/AiMultiModels.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+"use client"
+import React, { useContext, useState } from 'react'
 import AiModelList from '@/shared/AiModelList';
 import Image from "next/image";
 import {
@@ -11,14 +12,36 @@ import {
 import { Switch } from '@/components/ui/switch';
 import { MessageSquare, Lock } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { SelectGroup, SelectLabel } from '@radix-ui/react-select';
+import { AiSelectedModelConetxt } from '@/context/AiSelectedModelContext';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '@/config/FirebaseConfig';
+import { useUser } from '@clerk/nextjs';
 
 function AiMultiModels() {
+  const {user} = useUser();
   const [aiModelList,setAiModelList] = useState(AiModelList)
+  const {aiSelectedModels,setAiSelectedModels} = useContext(AiSelectedModelConetxt);
 
   const onToggleChange=(model, value) => {
     setAiModelList((prev) =>
         prev.map((m) =>
             m.model === model ? { ...m, enable: value } : m))
+  }
+
+  const onSelecteValue = async(parentModel,value)=>{
+    setAiSelectedModels(prev=>({
+        ...prev,
+        [parentModel]:{
+            modelId:value
+        }
+    }))
+
+    // Update to Firebase Database
+    const docRef = doc(db,"users",user?.primaryEmailAddress?.emailAddress)
+    await updateDoc(docRef,{
+        selectedModelPref:aiSelectedModels
+    })
   }
 
   return (
@@ -33,21 +56,33 @@ function AiMultiModels() {
                     width={24} height={24}
                     />
                     
-  {model.enable &&  <Select>
+  {model.enable &&  <Select defaultValue={aiSelectedModels[model.model].modelId} onValueChange={(value)=>onSelecteValue(model.model,value)} disabled={model.premium}>
                         <SelectTrigger className="w-[180px]">
-                            <SelectValue placeholder={model.subModel[0].name} />
+                            <SelectValue placeholder={aiSelectedModels[model.model].modelId} />
                         </SelectTrigger>
                         <SelectContent>
+                            <SelectGroup className='px-3'>
+                                <SelectLabel className='text-sm text-gray-400'>Free</SelectLabel>
+                                {model.subModel.map((subModel, index) => subModel.premium == false && (
+                                    <SelectItem key={index} value={subModel.id}>
+                                        {subModel.name}
+                                    </SelectItem>
+                                ))}
+                            </SelectGroup>
 
-                            {model.subModel.map((subModel, index)=>(
-                                <SelectItem key={index} value={subModel.name}>{subModel.name}</SelectItem>
-                            ))}
-
+                            <SelectGroup className='px-3'>
+                                <SelectLabel className='text-sm text-gray-400'>Premium</SelectLabel>
+                                {model.subModel.map((subModel, index) => subModel.premium == true && (
+                                    <SelectItem key={index} value={subModel.name} disabled={subModel.premium} >
+                                        {subModel.name} {subModel.premium && <Lock className='h-4 w-4'/>}
+                                    </SelectItem>
+                                ))}
+                            </SelectGroup>
                         </SelectContent>
                     </Select>   }
                 </div>
                 <div>
-    { model.enable? <Switch checked={model.enable} 
+            { model.enable? <Switch checked={model.enable} 
                     onCheckedChange={(v) => onToggleChange(model.model,v) }
                     /> : <MessageSquare onClick={()=> onToggleChange(model.model,true) }/> }
                 </div>
